1163afa72e2bdaf487d521a0e777a46b23da6ffa
Sheridan <sdb358@stern.nyu.edu>
2019-06-18 22:33:18 -0400

    get close price

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -2,6 +2,9 @@
 import requests
 import json
 
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price)
+
 #INFORMATION INPUTS
 
 request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
@@ -14,6 +17,9 @@ print(response.text)
 parsed_response = json.loads(response.text)
 
 last_refreshed = parsed_response["Meta Data"] ["3. Last Refreshed"]
+
+latest_close = parsed_response ["Time Series (Daily)"] ["2019-02-20"] ["4. close"]
+
 #breakpoint()
 #INFORMATION OUTPUTS
 
@@ -23,10 +29,10 @@ print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+print("REQUEST AT: 2018-02-20 02:00pm")##INSERT DATE TIME ##
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
